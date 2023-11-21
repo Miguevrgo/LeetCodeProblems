1eaf745a5a5eb6517accca678ad55694f282e14e
Miguevrgo <miguevrgo@gmail.com>
2023-11-21 21:07:21 +0100

    Upload solution problem 4

---
+++ 4.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+
+        // Sort the merged vector
+        sort(nums1.begin(), nums1.end());
+        
+        // Find the median
+        int length = nums1.size();
+        if (length % 2 == 0) {
+            return (nums1[length / 2 - 1] + nums1[length / 2]) / 2.0;
+        } else {
+            return nums1[length / 2];
+        }
+    }
+};
