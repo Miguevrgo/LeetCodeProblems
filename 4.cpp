0dc8ef8f16b1ac9d515179d5f356888e9518b2c6
Miguevrgo <miguevrgo@gmail.com>
2023-11-22 15:23:53 +0100

    4 upload

--- 4.cpp
+++ 4.cpp
@@ -8,7 +8,6 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-
         // Sort the merged vector
         sort(nums1.begin(), nums1.end());
         
