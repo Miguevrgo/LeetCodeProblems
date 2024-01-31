de82c23b9b68191d5d85d86088379b305669a8cf
miguevr <miguevrgo@gmail.com>
2024-01-31 13:30:59 +0100

    Add .h to git

---
+++ includeDir.h
@@ -0,0 +1,24 @@
+//
+// Created by miguevr on 1/31/24.
+//
+
+#ifndef LEETCODEPROBLEMS_INCLUDEDIR_H
+#define LEETCODEPROBLEMS_INCLUDEDIR_H
+
+
+
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <utility>
+#include <list>
+
+using namespace std;
+
+
+
+#endif //LEETCODEPROBLEMS_INCLUDEDIR_H
