cc79f983f6d2514458a229958b6c18b97d6d5c93
miguevrgo <miguevrgo@gmail.com>
2024-06-19 18:16:23 +0200

    Update 1

--- 1.cpp
+++ 1.cpp
@@ -1,56 +1,16 @@
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-using namespace std;
-
-// Descomentar para la solución eficiente
-// #define EFFICIENCY 
-
-#ifndef EFFICIENCY
-/**
- * @brief Resuelve el problema con eficiencia O(n²) que no es lo mejor, sin embargo dado mi conocimiento actual, la implementacion
- * con el arbol es desconocida
-*/
+#include <bits/stdc++.h>
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> used;
-        for (auto it = nums.begin(); it != nums.end(); ++it) {
-            int current = *it;
-            int dist = target - current;
-
-            auto pos = find(used.begin(), used.end(), dist);
-            if (pos != used.end()) {
-                vector<int> solution;
-                solution.push_back(distance(nums.begin(), it));
-                solution.push_back(distance(used.begin(), pos));
-                return solution;
-            }
-            used.push_back(current);
-        }
-        return vector<int>();
-    }
-};
-
-#endif
-#ifdef EFFICIENCY
-
-/**
- * @brief Esta es la implementacion O(n)
-*/
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numToIndex;
+        std::unordered_map<int, int> need;
         for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            if (numToIndex.find(complement) != numToIndex.end()) {
-                return {numToIndex[complement], i};
+            auto it = need.find(nums[i]);
+            if (it != need.end()) {
+                return {it->second, i};
             }
-            numToIndex[nums[i]] = i;
+            need[target - nums[i]] = i;
         }
-        return {}; // En caso de no encontrar la suma deseada.
+
+        return {};
     }
 };
-
-#endif
