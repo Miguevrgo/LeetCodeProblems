2d70a52743ff710cfa483156b6d66ce803212737
Miguevrgo <Miguevrgo@gmail.com>
2024-08-02 11:11:04 +0200

    Upload 9.cpp better solution

--- 9.cpp
+++ 9.cpp
@@ -18,3 +18,18 @@ public:
         return true;
     }
 };
+
+    bool isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int rev = 0;
+
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == rev) || (x == rev / 10);
+    }
