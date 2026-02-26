8004e57ebc8c382fe6922b66553f61b161c6a91a
Miguevrgo <miguevrgo@gmail.com>
2026-02-26 12:36:35 +0100

    Upload solution for problem 1603

---
+++ 1603.cpp
@@ -0,0 +1,13 @@
+#include <array>
+class ParkingSystem {
+public:
+  ParkingSystem(const int big, const int medium, const int small)
+      : slots({big, medium, small}) {}
+
+  bool addCar(const int carType) {
+    return std::max(0, slots[static_cast<std::size_t>(carType - 1)]--);
+  }
+
+private:
+  std::array<int, 3> slots;
+};
