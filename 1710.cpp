07c20c77a83f42dab20798d317e5f5607f432b18
Miguevrgo <miguevrgo@gmail.com>
2026-02-26 14:08:29 +0100

    Upload solution for problem 1710

---
+++ 1710.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  int maximumUnits(std::vector<std::vector<int>> &boxTypes, int truckSize) {
+    std::ranges::sort(boxTypes, std::greater{},
+                      [](const auto &elem) { return elem[1]; });
+
+    unsigned amount{0};
+    for (const auto &elem : boxTypes) {
+      amount += elem[1] * std::min(elem[0], truckSize);
+      truckSize -= elem[0];
+      if (truckSize <= 0) {
+        break;
+      }
+    }
+
+    return amount;
+  }
+};
